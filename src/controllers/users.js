e6f36927763b58244bf7b1068f8311e5bed44979
José Ithalo <jose_ithalo@hotmail.com>
2022-11-19 23:28:58 -0300

    Criptografia de senha realizada

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,5 @@
 const knex = require('../services/connection');
+const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
     const { username, password } = req.body;
@@ -15,21 +16,27 @@ const registerUser = async (req, res) => {
             return res.status(406).json('Este usuário já existe, não será possível cadastrá-lo');
         }
 
-        if (username.length < 3) {
-            return res.status(406).json('O seu username deve conter pelo menos 3 caracteres');
+        // if (username.length < 3) {
+        //     return res.status(406).json('O seu username deve conter pelo menos 3 caracteres');
+        // }
+
+        // if (password.length < 8) {
+        //     return res.status(406).json('A senha deve ser composta de pelo menos 8 caracteres.');
+        // }
+
+        // if (!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/)) {
+        //     return res.status(406).json('Utilize letras maiúsculas, minúsculas e números para criação da senha.');
+        // };
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await knex('users').insert({ username, password: encryptedPassword });
+
+        if (newUser.rowCount < 1) {
+            return res.status(400).json('O usuário não foi cadastrado.');
         }
 
-        if (password.length < 8) {
-            return res.status(406).json('A senha deve ser composta de pelo menos 8 caracteres.');
-        }
-
-        if (!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/)) {
-            return res.status(406).json('Utilize letras maiúsculas, minúsculas e números para criação da senha.');
-        };
-
-        await knex('users').insert({ username, password });
-
-        return res.status(200).json('Usuário cadastrada com sucesso!');
+        return res.status(201).json('Usuário cadastrada com sucesso!');
 
     } catch (error) {
         return res.status(400).json(error.message);
