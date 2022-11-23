646ee38d9c7753a3360f95eadba447e28cf56254
José Ithalo <jose_ithalo@hotmail.com>
2022-11-22 21:21:39 -0300

    Função de visualizar saldo realizada

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,8 +1,17 @@
 const knex = require('../services/connection');
 
 const viewBalance = async (req, res) => {
+    const { accountid } = req.user;
 
-    console.log('Account');
+    try {
+
+        const currentBalance = await knex('accounts').where({ id: accountid }).first();
+
+        return res.status(200).json('Seu saldo é de: R$' + (currentBalance.balance / 100).toFixed(2));
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 module.exports = {
