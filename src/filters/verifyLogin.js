d541c489b2bfdb61baa9a853df9f982a651bda5b
José Ithalo <jose_ithalo@hotmail.com>
2022-11-22 21:18:20 -0300

    Finalização do middleware de rota

--- src/filters/verifyLogin.js
+++ src/filters/verifyLogin.js
@@ -5,10 +5,30 @@ const jwt = require('jsonwebtoken');
 const verifyLogin = async (req, res, next) => {
     const { authorization } = req.headers;
 
-    console.log(authorization);
-
     if (!authorization || authorization === 'Bearer') {
-        return res.status(401).json('Você não possui autorização para isso');
+        return res.status(401).json('Você não possui autorização. Faça o login');
+    }
+
+    try {
+
+        const token = authorization.replace('Bearer', '').trim();
+
+        const { accountid } = jwt.verify(token, hashedPassword);
+
+        const loggedUser = await knex('users').where({ accountid }).first();
+
+        if (!loggedUser) {
+            return res.status(401).json('Acesso negado.');
+        }
+
+        const { password, ...userData } = loggedUser;
+
+        req.user = userData;
+
+        next();
+
+    } catch (error) {
+        return res.status(400).json(error.message);
     }
 
 }
