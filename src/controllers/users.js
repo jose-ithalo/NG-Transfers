c54535f63288abde4b70deb5345f9dd87d294a69
José Ithalo <jose_ithalo@hotmail.com>
2023-01-19 11:20:10 -0300

    Descomentei algus blocos de código

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,17 +16,17 @@ const registerUser = async (req, res) => {
             return res.status(406).json('Este usuário já existe, não será possível cadastrá-lo');
         }
 
-        // if (username.length < 3) {
-        //     return res.status(406).json('O seu username deve conter pelo menos 3 caracteres');
-        // }
+        if (username.length < 3) {
+            return res.status(406).json('O seu username deve conter pelo menos 3 caracteres');
+        }
 
-        // if (password.length < 8) {
-        //     return res.status(406).json('A senha deve ser composta de pelo menos 8 caracteres.');
-        // }
+        if (password.length < 8) {
+            return res.status(406).json('A senha deve ser composta de pelo menos 8 caracteres.');
+        }
 
-        // if (!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/)) {
-        //     return res.status(406).json('Utilize letras maiúsculas, minúsculas e números para criação da senha.');
-        // };
+        if (!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/)) {
+            return res.status(406).json('Utilize letras maiúsculas, minúsculas e números para criação da senha.');
+        };
 
         const encryptedPassword = await bcrypt.hash(password, 10);
 
